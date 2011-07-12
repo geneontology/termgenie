6299e0419965a39d02d78b73f0d23deee445ac3b
Heiko Dietze <hdietze@lbl.gov>
2011-07-12 20:56:56 +0000

    add option to remove qualifiers in clean up
    Former-commit-id: 2474fcd308a59fdfec11460d99a47041fc63cd43

--- TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/ontology/DefaultOntologyCleaner.java
+++ TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/ontology/DefaultOntologyCleaner.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.bbop.termgenie.ontology.DefaultOntologyCleaner.CleanerConfig.CleanDetails;
 import org.bbop.termgenie.tools.ResourceLoader;
 import org.obolibrary.oboformat.model.Clause;
 import org.obolibrary.oboformat.model.Frame;
@@ -23,22 +24,22 @@ public class DefaultOntologyCleaner extends ResourceLoader {
 	private final static Logger LOGGER = Logger.getLogger(DefaultOntologyCleaner.class);
 	private final static String SETTINGS_FILE = "default-ontology-cleaner.settings";
 	private volatile static DefaultOntologyCleaner instance = null;
-	
+
 	private final Map<String, CleanerConfig> cleanOntologies;
-	
+
 	private DefaultOntologyCleaner() {
 		super();
 		InputStream inputStream = loadResource(SETTINGS_FILE);
 		cleanOntologies = CleanerConfig.loadSettings(inputStream);
 	}
-	
+
 	private static final synchronized DefaultOntologyCleaner getInstance() {
 		if (instance == null) {
 			instance = new DefaultOntologyCleaner();
 		}
 		return instance;
 	}
-	
+
 	static void cleanOntology(String ontology, OBODoc obodoc) {
 		if (obodoc != null) {
 			CleanerConfig cleanerConfig = getInstance().cleanOntologies.get(ontology);
@@ -48,14 +49,14 @@ public class DefaultOntologyCleaner extends ResourceLoader {
 			}
 		}
 	}
-	
+
 	private static void cleanOBO(OBODoc obodoc, CleanerConfig config) {
 		cleanFrames(obodoc.getTermFrames(), config.retain_term_clauses);
 		cleanFrames(obodoc.getInstanceFrames(), config.retain_instance_clauses);
 		cleanFrames(obodoc.getTypedefFrames(), config.retain_typedef_clauses);
 	}
-	
-	private static void cleanFrames(Collection<Frame> frames, Map<String, Set<String>> retains) {
+
+	private static void cleanFrames(Collection<Frame> frames, Map<String, CleanDetails> retains) {
 		for (Frame frame : frames) {
 			Collection<Clause> clauses = frame.getClauses();
 			Iterator<Clause> iterator = clauses.iterator();
@@ -64,37 +65,52 @@ public class DefaultOntologyCleaner extends ResourceLoader {
 				if (!keepClause(clause, retains)) {
 					iterator.remove();
 				}
+				else {
+					cleanClause(clause, retains);
+				}
 			}
 		}
 	}
-	
-	private static boolean keepClause(Clause clause, Map<String, Set<String>> retains) {
+
+	private static void cleanClause(Clause clause, Map<String, CleanDetails> retains) {
 		String tag = clause.getTag();
-		Set<String> set = retains.get(tag);
-		if (set == null) {
-			return false;
-		}
-		else if (set.isEmpty()) {
-			return true;
-		}
-		else {
-			String value = clause.getValue().toString();
-			return set.contains(value);
+		CleanDetails details = retains.get(tag);
+		if (details.clearQualifiers) {
+			clause.getQualifierValues().clear();
 		}
 	}
-	
+
+	private static boolean keepClause(Clause clause, Map<String, CleanDetails> retains) {
+		String tag = clause.getTag();
+		CleanDetails details = retains.get(tag);
+		if (details == null) {
+			return false;
+		} else if (details.types.isEmpty()) {
+			return true;
+		} else {
+			String value = clause.getValue().toString();
+			return details.types.contains(value);
+		}
+	}
+
 	static class CleanerConfig {
-		Map<String, Set<String>> retain_term_clauses = new HashMap<String, Set<String>>();
-		Map<String, Set<String>> retain_instance_clauses = new HashMap<String, Set<String>>();
-		Map<String, Set<String>> retain_typedef_clauses = new HashMap<String, Set<String>>();
-		
+
+		public static class CleanDetails {
+			Set<String> types = Collections.emptySet();
+			boolean clearQualifiers = false;
+		}
+
+		Map<String, CleanDetails> retain_term_clauses = new HashMap<String, CleanDetails>();
+		Map<String, CleanDetails> retain_instance_clauses = new HashMap<String, CleanDetails>();
+		Map<String, CleanDetails> retain_typedef_clauses = new HashMap<String, CleanDetails>();
+
 		static Map<String, CleanerConfig> loadSettings(InputStream inputStream) {
 			Map<String, CleanerConfig> settings = new HashMap<String, CleanerConfig>();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 			String line;
-			
+
 			CleanerConfig currentConfig = null;
-			Map<String, Set<String>> currentMap = null;
+			Map<String, CleanDetails> currentMap = null;
 			try {
 				while ((line = reader.readLine()) != null) {
 					line = line.trim();
@@ -104,69 +120,95 @@ public class DefaultOntologyCleaner extends ResourceLoader {
 					if (line.equals("[Ontology]")) {
 						currentConfig = new CleanerConfig();
 						currentMap = null;
-					}
-					else if (line.startsWith("name:")) {
+					} else if (line.startsWith("name:")) {
 						if (currentConfig != null) {
 							String name = line.substring(5).trim();
 							settings.put(name, currentConfig);
 						}
-					}
-					else if (line.equals("[Term]")) {
+					} else if (line.equals("[Term]")) {
 						if (currentConfig != null) {
 							currentMap = currentConfig.retain_term_clauses;
 						}
-					}
-					else if (line.equals("[Typdef]")) {
+					} else if (line.equals("[Typdef]")) {
 						if (currentConfig != null) {
 							currentMap = currentConfig.retain_typedef_clauses;
 						}
-					}
-					else if (line.equals("[Instance]")) {
+					} else if (line.equals("[Instance]")) {
 						if (currentConfig != null) {
 							currentMap = currentConfig.retain_instance_clauses;
 						}
-					}
-					else {
+					} else {
 						if (currentMap != null) {
-							int pos = line.indexOf('[');
-							String tag = null;
-							Set<String> set = Collections.emptySet();
-							if (pos > 0) {
-								tag = line.substring(0, pos).trim();
-								String list = line.substring(pos);
-								// split
-								set = new HashSet<String>();
-								int prevPos = 1;
-								for(int i=prevPos; i<list.length(); i++) {
-									char c = list.charAt(i);
-									if (c == ',' || c == ']') {
-										if (i > prevPos) {
-											set.add(list.substring(prevPos, i).trim());
-										}
-										prevPos = i + 1;
-									}
-								}
-							}
-							else if (pos < 0) {
-								tag = line;
-							}
-							if (tag != null) {
-								currentMap.put(tag, set);
-							}
+							parseLine(line, currentMap);
 						}
 					}
 				}
 				return settings;
 			} catch (IOException exception) {
 				throw new RuntimeException(exception);
-			}
-			finally {
+			} finally {
 				try {
 					reader.close();
 				} catch (IOException exception) {
 					LOGGER.error("unable to close reader", exception);
 				}
 			}
+		}
+
+		private static void parseLine(String line, Map<String, CleanDetails> currentMap) {
+			int spacePos = findWhitespace(line, 0);
+			String tag = null;
+			CleanDetails details = new CleanDetails();
+			if (spacePos < 0) {
+				tag = line;
+			} else {
+				tag = line.substring(0, spacePos);
+				int startListPos = line.indexOf('[', spacePos);
+				int endListPos = line.indexOf(']', startListPos);
+				if (startListPos > 0) {
+					if (endListPos > 0) {
+						details.types = parseList(line.substring(startListPos, endListPos + 1));
+						spacePos = findWhitespace(line, endListPos);
+					} else {
+						// irregular line, remove
+						tag = null;
+						spacePos = -1;
+					}
+				}
+				if (spacePos > 0) {
+					String clearQualifiersString = line.substring(spacePos).trim();
+					if ("true".equals(clearQualifiersString)) {
+						details.clearQualifiers = true;
+					}
+				}
+			}
+			if (tag != null) {
+				currentMap.put(tag, details);
+			}
+		}
+
+		private static Set<String> parseList(String s) {
+			Set<String> set = new HashSet<String>();
+			int prevPos = 1;
+			for (int i = prevPos; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if (c == ',' || c == ']') {
+					if (i > prevPos) {
+						set.add(s.substring(prevPos, i).trim());
+					}
+					prevPos = i + 1;
+				}
+			}
+			return set;
+		}
+
+		private static int findWhitespace(String s, int start) {
+			for (int i = start; i < s.length(); i++) {
+				if (Character.isWhitespace(s.charAt(i))) {
+					return i;
+				}
+			}
+			return -1;
 		}
 	}
 }
