a96a3ff43d931851af489685fad1eb19a2695940
Heiko Dietze <hdietze@lbl.gov>
2011-07-26 17:28:34 +0000

    update export to use existing OBO writer
    Former-commit-id: 1c7c2fda72a89c95cbfe42f630162cf38359e286

--- TermGenie/TermGenieJQuery/src/main/java/org/bbop/termgenie/services/TermCommitServiceImpl.java
+++ TermGenie/TermGenieJQuery/src/main/java/org/bbop/termgenie/services/TermCommitServiceImpl.java
@@ -1,10 +1,25 @@
 package org.bbop.termgenie.services;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.apache.log4j.Logger;
 import org.bbop.termgenie.data.JsonOntologyTerm;
 import org.bbop.termgenie.data.JsonOntologyTerm.JsonSynonym;
+import org.bbop.termgenie.data.JsonOntologyTerm.JsonTermMetaData;
+import org.bbop.termgenie.data.JsonOntologyTerm.JsonTermRelation;
 import org.bbop.termgenie.ontology.OntologyTaskManager;
 import org.bbop.termgenie.ontology.OntologyTaskManager.OntologyTask;
 import org.bbop.termgenie.tools.OntologyTools;
+import org.obolibrary.oboformat.model.Clause;
+import org.obolibrary.oboformat.model.Frame;
+import org.obolibrary.oboformat.model.Frame.FrameType;
+import org.obolibrary.oboformat.model.FrameMergeException;
+import org.obolibrary.oboformat.model.OBODoc;
+import org.obolibrary.oboformat.model.Xref;
+import org.obolibrary.oboformat.parser.OBOFormatConstants.OboFormatTag;
+import org.obolibrary.oboformat.writer.OBOFormatWriter;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -14,6 +29,8 @@ import owltools.graph.OWLGraphWrapper;
 @Singleton
 public class TermCommitServiceImpl implements TermCommitService {
 
+	private static final Logger logger = Logger.getLogger(TermCommitServiceImpl.class);
+	
 	private final OntologyTools ontologyTools;
 	
 	/**
@@ -35,108 +52,131 @@ public class TermCommitServiceImpl implements TermCommitService {
 			result.setMessage("Unknown ontology: "+ontologyName);
 			return result;
 		}
-		
-		// TODO use a proper obo export tool here!
-		final StringBuilder sb = new StringBuilder();
-		sb.append("Preliminary export results:<br/>");
-		sb.append("<pre>\n");
+		String ontologyIdPrefix = getOntologyIdPrefix(manager);
+		int count = 0;
+		OBODoc doc = new OBODoc();
+		doc.setHeaderFrame(new Frame(FrameType.HEADER));
 		for (JsonOntologyTerm term : terms) {
-			sb.append("[Term]\n");
-			manager.runManagedTask(new OntologyTask(){
-
-				@Override
-				public void run(OWLGraphWrapper managed) {
-					String id = managed.getOntologyId();
-					sb.append("id: ");
-					sb.append(id);
-					sb.append(":-------\n");
-				}
-			});
-			sb.append("name: ");
-			sb.append(term.getLabel());
-			sb.append('\n');
-			sb.append("def: \"");
-			sb.append(term.getDefinition());
-			sb.append("\"");
+			final Frame frame  = new Frame(FrameType.TERM);
+			
+			// id
+			frame.setId(createFakeId(ontologyIdPrefix, count));
+			
+			// name
+			addClause(frame, OboFormatTag.TAG_NAME, term.getLabel());
+			
+			// definition
+			Clause defClause = create(OboFormatTag.TAG_DEF, term.getDefinition());
+			frame.addClause(defClause);
 			String[] defxRefs = term.getDefxRef();
 			if (defxRefs != null && defxRefs.length > 0) {
-				sb.append(" [");
-				for (int i = 0; i < defxRefs.length; i++) {
-					if (i > 0) {
-						sb.append(", ");
-					}
-					sb.append(defxRefs[i]);
+				for (String defxRef : defxRefs) {
+					Xref xref = new Xref(defxRef);
+					defClause.addXref(xref);
 				}
-				sb.append("]\n");
 			}
-			else {
-				sb.append('\n');
-			}
-			final String comment = term.getMetaData().getComment();
-			if (comment != null && !comment.isEmpty()) {
-				sb.append("comment: \"");
-				sb.append(comment);
-				sb.append("\"\n");
-			}
+
+			// synonyms
 			JsonSynonym[] synonyms = term.getSynonyms();
-			if (synonyms != null && synonyms.length > 0) {
-				for (JsonSynonym synonym : synonyms) {
-					sb.append("synonym: \"");
-					sb.append(synonym.getLabel());
-					sb.append('"');
-					String scope = synonym.getScope();
-					if (scope != null && !scope.isEmpty()) {
-						sb.append(' ');
-						sb.append(scope);
-					}
-					String category = synonym.getCategory();
-					if (category != null && !category.isEmpty()) {
-						sb.append(' ');
-						sb.append(category);
-					}
-					String[] xrefs = synonym.getXrefs();
-					if (xrefs != null && xrefs.length > 0) {
-						for (int i = 0; i < xrefs.length; i++) {
-							if (i == 0) {
-								sb.append(" [");
-							}
-							else {
-								sb.append(',');
-							}
-							sb.append(xrefs[i]);
+			if (synonyms != null) {
+				for (JsonSynonym jsonSynonym : synonyms) {
+					Clause synClause = create(OboFormatTag.TAG_SYNONYM, jsonSynonym.getLabel());
+					String[] xrefs = jsonSynonym.getXrefs();
+					if (xrefs != null) {
+						for (String xref : xrefs) {
+							synClause.addXref(new Xref(xref));
 						}
-						sb.append(']');
 					}
-					
-					sb.append("\n");
+					String scope = jsonSynonym.getScope();
+					if (scope != null) {
+						synClause.addValue(scope);
+					}
+					frame.addClause(synClause);
 				}
 			}
 			
-			/*
-	private String logDef;
-	private JsonTermRelation[] relations;
-			 */
-//			is_a
-//			intersection_of
-//			union_of
-//			disjoint_from
-//			relationship
-			sb.append("created_by: ");
-			sb.append(term.getMetaData().getCreated_by());
-			sb.append('\n');
-			sb.append("creation_date: ");
-			sb.append(term.getMetaData().getCreation_date());
-			sb.append('\n');
-			sb.append('\n');
+			// relations
+			JsonTermRelation[] relations = term.getRelations();
+			if (relations != null) {
+				for (JsonTermRelation relation : relations) {
+					// TODO write relations
+				}
+			}
+			
+			// meta data
+			JsonTermMetaData metaData = term.getMetaData();
+			addClause(frame, OboFormatTag.TAG_COMMENT, metaData.getComment());
+			addClause(frame, OboFormatTag.TAG_CREATED_BY, metaData.getCreated_by());
+			addClause(frame, OboFormatTag.TAG_CREATION_DATE, metaData.getCreation_date());
+			
+			
+			try {
+				doc.addTermFrame(frame);
+			} catch (FrameMergeException exception) {
+				result.setSuccess(false);
+				result.setMessage("Could not create OBO export: "+exception.getMessage());
+				return result;
+			}
+			count += 1;
 		}
-		sb.append("\n</pre>");
-		result.setSuccess(true);
-		result.setFormats(new String[]{"OBO"});
-		result.setContents(new String[]{sb.toString()});
-		// TODO add OWL export support
-		return result;
+		
+		OBOFormatWriter writer = new OBOFormatWriter();
+		try {
+			StringWriter stringWriter = new StringWriter();
+			BufferedWriter bwriter = new BufferedWriter(stringWriter);
+			writer.write(doc, bwriter);
+			bwriter.close();
+			result.setSuccess(true);
+			result.setFormats(new String[]{"OBO"});
+			result.setContents(new String[]{stringWriter.getBuffer().toString()});
+			// TODO add OWL export support
+			return result;
+		} catch (IOException exception) {
+			logger.error("Could not create export results.", exception);
+			result.setSuccess(false);
+			result.setMessage("Could not write OBO export: "+exception.getMessage());
+			return result;
+		}
 	}
 	
+	private void addClause(Frame frame, OboFormatTag tag, String value) {
+		if (value != null && !value.isEmpty()) {
+			frame.addClause(create(tag, value));
+		}
+	}
+	
+	private Clause create(OboFormatTag tag, String value) {
+		return new Clause(tag.toString(), value);
+	}
+	
+	private String createFakeId(String prefix, int count) {
+		StringBuilder sb = new StringBuilder(prefix);
+		sb.append(":fake");
+		if (count < 100) {
+			sb.append('0');
+		}
+		if (count < 10) {
+			sb.append('0');
+		}
+		sb.append(count);
+		return sb.toString();
+	}
+	
+	private String getOntologyIdPrefix(OntologyTaskManager manager) {
+		LocalTask task = new LocalTask();
+		manager.runManagedTask(task);
+		return task.id;
+	}
+	
+	private final class LocalTask implements OntologyTask {
+		String id = null;
+	
+		@Override
+		public void run(OWLGraphWrapper managed) {
+			id = managed.getOntologyId();
+		}
+	}
+
 	@Override
 	public JsonCommitResult commitTerms(String sessionId, JsonOntologyTerm[] terms, String ontology) {
 		JsonCommitResult result = new JsonCommitResult();
