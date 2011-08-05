49923a0880a790b828c446b82837fce5d65d1862
Heiko Dietze <hdietze@lbl.gov>
2011-08-05 01:05:35 +0000

    enable owl loading for ontology loader
    Former-commit-id: 5a9ee76d616e4981ba43fb766ac97d3d94ba8cbf

--- TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/ontology/impl/ReloadingOntologyLoader.java
+++ TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/ontology/impl/ReloadingOntologyLoader.java
@@ -24,9 +24,12 @@ import org.bbop.termgenie.ontology.impl.DefaultOntologyConfiguration.ConfiguredO
 import org.obolibrary.obo2owl.Obo2Owl;
 import org.obolibrary.oboformat.model.OBODoc;
 import org.obolibrary.oboformat.parser.OBOFormatParser;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLObject;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.UnknownOWLOntologyException;
 
 import owltools.graph.OWLGraphWrapper;
@@ -170,7 +173,7 @@ public class ReloadingOntologyLoader implements OntologyLoader {
 			return null;
 		}
 		for(String support : ontology.getSupports()) {
-			OWLOntology owl = loadOWL("support", support);
+			OWLOntology owl = loadOntology("support", support);
 			if (support != null) {
 				w.addSupportOntology(owl);
 			}
@@ -178,8 +181,8 @@ public class ReloadingOntologyLoader implements OntologyLoader {
 		return w;
 	}
 	
-	OWLGraphWrapper load(Ontology ontology, String url) throws OWLOntologyCreationException, IOException {
-		OWLOntology owlOntology = loadOWL(ontology.getUniqueName(), url);
+	protected OWLGraphWrapper load(Ontology ontology, String url) throws OWLOntologyCreationException, IOException {
+		OWLOntology owlOntology = loadOntology(ontology.getUniqueName(), url);
 		if (owlOntology == null) {
 			return null;
 		}
@@ -188,7 +191,7 @@ public class ReloadingOntologyLoader implements OntologyLoader {
 
 	
 	
-	protected OWLOntology loadOWL(String ontology, String url) throws OWLOntologyCreationException, IOException {
+	protected OWLOntology loadOntology(String ontology, String url) throws OWLOntologyCreationException, IOException {
 		LOGGER.info("Loading ontology: "+ontology+"  baseURL: "+url);
 		URL realUrl;
 		if (iriMapper != null) {
@@ -197,6 +200,21 @@ public class ReloadingOntologyLoader implements OntologyLoader {
 		else {
 			realUrl = new URL(url);
 		}
+		String lowerCaseUrl = url.toLowerCase();
+		if (lowerCaseUrl.endsWith(".obo")) {
+			return loadOBO2OWL(ontology, realUrl);
+		}
+		else if (lowerCaseUrl.endsWith(".owl")) {
+			OWLOntologyManager manager = OWLManager.createOWLOntologyManager(); // persist?
+			return manager.loadOntologyFromOntologyDocument(IRI.create(url));
+		}
+		else {
+			throw new RuntimeException("Unable to load ontology from url, as no known suffix ('.obo' or '.owl') was detected: "+url); 
+		}
+	}
+
+	protected OWLOntology loadOBO2OWL(String ontology, URL realUrl) throws IOException,
+			OWLOntologyCreationException {
 		OBOFormatParser p = new OBOFormatParser();
 		OBODoc obodoc;
 		try {
