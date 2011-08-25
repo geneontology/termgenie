f53f1b6d778691d9fe7a72e1a7707a72fe1051a6
Heiko Dietze <hdietze@lbl.gov>
2011-08-25 19:53:35 +0000

    add module option for the default reasoner
    Former-commit-id: da0d55abd594050fddc246a05ea5a3a2c1bed073

--- TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/core/rules/ReasonerModule.java
+++ TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/core/rules/ReasonerModule.java
@@ -7,13 +7,25 @@ import com.google.inject.name.Names;
 public class ReasonerModule extends IOCModule {
 
 	public static final String NAMED_DIRECT_REASONER_FACTORY = "DirectReasonerFactory";
+	private final String defaultReasonerName;
+	
+	/**
+	 * @param defaultReasonerName
+	 */
+	public ReasonerModule(String defaultReasonerName) {
+		super();
+		this.defaultReasonerName = defaultReasonerName;
+	}
+
+	public ReasonerModule() {
+		this(ReasonerFactoryImpl.JCEL);
+	}
 
 	@Override
 	protected void configure() {
 		bind(ReasonerFactory.class).to(CachingReasonerFactoryImpl.class);
-		bind(ReasonerFactoryImpl.REASONER_FACTORY_DEFAULT_REASONER, ReasonerFactoryImpl.JCEL);
+		bind(ReasonerFactoryImpl.REASONER_FACTORY_DEFAULT_REASONER, defaultReasonerName);
 		bind(ReasonerFactory.class).annotatedWith(Names.named(NAMED_DIRECT_REASONER_FACTORY)).to(ReasonerFactoryImpl.class);
 	}
-	
 	
 }
