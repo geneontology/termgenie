ca8e446712b41d7aee50c0b8f97dc12c34670ad2
Heiko Dietze <hdietze@lbl.gov>
2011-08-25 17:33:27 +0000

    add content for spin
    Former-commit-id: 94ca22c256edc15420c1069bc4850ee2796f6888

---
+++ TermGenie/WebApplications/TermGenieWebAppCL/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppCLServlet.java
@@ -0,0 +1,45 @@
+package org.bbop.termgenie.servlets;
+
+import org.bbop.termgenie.core.io.XMLTermTemplateModule;
+import org.bbop.termgenie.core.ioc.IOCModule;
+import org.bbop.termgenie.ontology.OntologyConfiguration;
+import org.bbop.termgenie.ontology.impl.ReloadingOntologyModule;
+import org.bbop.termgenie.ontology.impl.XMLOntologyConfiguration;
+import org.bbop.termgenie.rules.DefaultDynamicRulesModule;
+
+public class TermGenieWebAppCLServlet extends AbstractJsonRPCServlet {
+
+	// generated
+	private static final long serialVersionUID = 6676166543254873837L;
+
+	@Override
+	protected ServiceExecutor createServiceExecutor() {
+		return new ServiceExecutor() {
+
+			@Override
+			protected IOCModule getOntologyModule() {
+				return new ReloadingOntologyModule() {
+
+					@Override
+					protected void bindOntologyConfiguration() {
+						bind(OntologyConfiguration.class).to(XMLOntologyConfiguration.class);
+						bind("XMLOntologyConfigurationResource", "ontology-configuration_cl.xml");
+					}
+				};
+			}
+
+			@Override
+			protected IOCModule getRulesModule() {
+				return new DefaultDynamicRulesModule() {
+
+					@Override
+					protected void bindTemplateIO() {
+						install(new XMLTermTemplateModule());
+						bind("DynamicRulesTemplateResource", "termgenie_rules_cl.xml");
+					}
+				};
+			}
+		};
+	}
+
+}
