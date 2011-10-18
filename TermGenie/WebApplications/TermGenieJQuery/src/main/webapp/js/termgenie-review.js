f34b2d7f3db2aa1dd036e89a1f507a13455da3b0
Heiko Dietze <hdietze@lbl.gov>
2011-10-18 00:04:21 +0000

    commit review: render history items with js
    Former-commit-id: 72644707391c640f90fca0a6e1fa67c937fac9b2

--- TermGenie/WebApplications/TermGenieJQuery/src/main/webapp/js/termgenie-review.js
+++ TermGenie/WebApplications/TermGenieJQuery/src/main/webapp/js/termgenie-review.js
@@ -113,14 +113,85 @@ function termgenieReview(){
 	}
 	
 	function createCommitReviewPanel(entries) {
+		// empty the current content
+		mainReviewPanel.empty();
 		
+		var checkboxes = [];
+		
+		var table = jQuery('<table></table>');
+		
+		// setup list of entries
+		jQuery.each(entries, function(index, entry){
+			var tr = jQuery('<tr></tr>');
+			
+			// checkbox
+			var td1 = jQuery('<td></td>');
+			tr.append(td1);
+			var checkbox = jQuery('<input type="checkbox" />');
+			td1.append(checkbox);
+			checkboxes.push(checkbox);
+			
+			// details
+			var td2 = jQuery('<td></td>');
+			tr.append(td2);
+			
+			// user and date
+			if (entry.user && entry.user.length > 0) {
+				td.append('<div><span>User</span><span>'+entry.user+'</span></div>');
+			}
+			if (entry.date && entry.date.length > 0) {
+				td.append('<div><span>Date</span><span>'+entry.date+'</span></div>');
+			}
+			
+			// diffs
+			var diffTable = jQuery('<table></table>');
+			
+			jQuery.each(entry.diffs, function(diffIndex, diff){
+				var diffTr = jQuery('<tr></tr>');
+				diffTable.append(diffTr);
+				var diffTd = jQuery('<td></td>');
+				diffTr.append(diffTd);
+				if (diff.operation === 0) {
+					// add
+					diffTd.append('<span>Add</span>');
+				}
+				else if (diff.operation === 1) {
+					// modify
+					diffTd.append('<span>Modify</span>');
+				}
+				else if (diff.operation === 2) {
+					// remove
+					diffTd.append('<span>Remove</span>');
+				}
+				diffTr.append(jQuery('<td><pre>'+diff.diff+'</pre></td>'));
+			});
+			
+			else {
+				
+			}	td.append(diffTable);
+			
+			table.append(tr);
+		});
 		
 		addCommitButton(function(){ // onClick
+			var reviewedEntries = [];
+			
 			// retrieved marked entries
-			
-			// create array
-			
-			// make rpc call
+			jQuery.each(entries, function(index, entry){
+				var isSelected = checkboxes[index].is(':checked');
+				if (isSelected === true) {
+					reviewedEntries.push(entry);
+				}
+			});
+			if (reviewedEntries.length > 0) {
+				// set messages and lock commit button
+				
+				// commit
+				executeCommitOnServer(reviewedEntries);
+			}
+			else {
+				// set error message
+			}
 		});
 	}
 	
@@ -128,6 +199,27 @@ function termgenieReview(){
 		var commitButton = jQuery('<button>Commit</button>');
 		commitButton.click(onclick);
 		mainControlPanel.append(commitButton);
+	}
+	
+	function executeCommitOnServer(entries) {
+		// make rpc call
+		mySession.getSessionId(function(sessionId){
+			jsonService.review.commit({
+				params: [sessionId, entries],
+				onSuccess: function(result){
+					if (result && result.success && result.success === true) {
+						// set success message
+					}
+					else {
+						// set error message
+					}
+				},
+				onException: function(e) {
+					jQuery.logSystemError('Could not commit terms.',e);
+					return true;
+				}
+			});	
+		});
 	}
 	
 	return {
