f3ed92b28e5e03e1270a835268fa15a7542fddf5
Heiko Dietze <hdietze@lbl.gov>
2011-11-18 22:02:01 +0000

    add new project for user management
    Former-commit-id: 1abb10f4cdd5b89a339969c6e3a38924640f27f9

---
+++ TermGenie/TermGenieUserManagement/TermGenieUserManagement-API/src/main/java/org/bbop/termgenie/user/UserData.java
@@ -0,0 +1,30 @@
+package org.bbop.termgenie.user;
+
+public interface UserData {
+
+	/**
+	 * @return the screenname
+	 */
+	public String getScreenname();
+
+	/**
+	 * @return the guid
+	 */
+	public String getGuid();
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail();
+
+	/**
+	 * @return the xref
+	 */
+	public String getXref();
+
+	/**
+	 * @return the scmAlias
+	 */
+	public String getScmAlias();
+
+}
---
+++ TermGenie/TermGenieUserManagement/TermGenieUserManagement-API/src/main/java/org/bbop/termgenie/user/UserDataProvider.java
@@ -0,0 +1,11 @@
+package org.bbop.termgenie.user;
+
+import java.util.List;
+
+
+public interface UserDataProvider {
+
+	public UserData getUserDataPerEMail(String email);
+	
+	public UserData getUserDataPerGuid(String guid, List<String> email);
+}
