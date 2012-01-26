b731df1aa9e381ab314dedf8d7ef0058076c8556
Heiko Dietze <hdietze@lbl.gov>
2012-01-26 17:34:22 +0000

    improve documentation and re-start behavior
    Former-commit-id: c0883de878d0ff04633096dae1a629f7ce2c6313

--- TermGenie/TermGenieOntologyCommit/TermGenieOntologyCommit-CVS/src/main/java/org/bbop/termgenie/ontology/impl/CvsAwareIRIMapper.java
+++ TermGenie/TermGenieOntologyCommit/TermGenieOntologyCommit-CVS/src/main/java/org/bbop/termgenie/ontology/impl/CvsAwareIRIMapper.java
@@ -4,13 +4,40 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.bbop.termgenie.cvs.CvsTools;
 import org.bbop.termgenie.ontology.IRIMapper;
 
+import com.google.inject.Singleton;
+
+/**
+ * {@link IRIMapper} which maps files from a cvs repository.<br/>
+ * Uses a fall-back {@link IRIMapper} to map unknown IRIs.
+ * <br/>
+ * WARNING: Do not create multiple instances with the same workFolder. May
+ * lead to strange side effects, as the wokrFolder is cleared in the setup process.
+ */
+@Singleton
 public class CvsAwareIRIMapper extends AbstractScmAwareIRIMapper<CvsAwareIRIMapper.CvsHandler> {
 
-	public CvsAwareIRIMapper(IRIMapper fallBackIRIMapper, String cvsRoot, String cvsPassword, File workFolder, Map<String, String> mappedCVSFiles, String checkout) {
+	/**
+	 * @param fallBackIRIMapper
+	 * @param cvsRoot
+	 * @param cvsPassword
+	 * @param workFolder
+	 * @param mappedCVSFiles
+	 * @param checkout
+	 */
+	// Do not provide an IOC constructor, as there are multiple implementations
+	// use @Provides methods instead
+	public CvsAwareIRIMapper(IRIMapper fallBackIRIMapper,
+			String cvsRoot,
+			String cvsPassword,
+			File workFolder,
+			Map<String, String> mappedCVSFiles,
+			String checkout)
+	{
 		super(fallBackIRIMapper, new CvsHandler(cvsRoot, cvsPassword, workFolder, mappedCVSFiles, checkout));
 	}
 
@@ -18,16 +45,24 @@ public class CvsAwareIRIMapper extends AbstractScmAwareIRIMapper<CvsAwareIRIMapp
 
 		private final CvsTools cvs;
 		private final Map<String, String> mappedCVSFiles;
-		
-		CvsHandler(String cvsRoot, String cvsPassword, File targetFolder, Map<String, String> mappedCVSFiles, String checkout) {
+
+		CvsHandler(String cvsRoot,
+				String cvsPassword,
+				File targetFolder,
+				Map<String, String> mappedCVSFiles,
+				String checkout)
+		{
 			this.mappedCVSFiles = mappedCVSFiles;
 			cvs = new CvsTools(cvsRoot, cvsPassword, targetFolder);
 			try {
+				// always clean the work directory.
+				FileUtils.cleanDirectory(targetFolder);
 				cvs.connect();
 				cvs.checkout(checkout);
 			} catch (IOException exception) {
 				throw new RuntimeException(exception);
-			} finally {
+			}
+			finally {
 				try {
 					cvs.close();
 				} catch (IOException exception) {
@@ -35,7 +70,7 @@ public class CvsAwareIRIMapper extends AbstractScmAwareIRIMapper<CvsAwareIRIMapp
 				}
 			}
 		}
-		
+
 		@Override
 		public void update(String url) throws IOException {
 			String cvsFile = mappedCVSFiles.get(url);
@@ -44,7 +79,8 @@ public class CvsAwareIRIMapper extends AbstractScmAwareIRIMapper<CvsAwareIRIMapp
 				cvs.update(cvsFile);
 			} catch (IOException exception) {
 				throw exception;
-			} finally {
+			}
+			finally {
 				try {
 					cvs.close();
 				} catch (Exception exception) {
@@ -61,10 +97,8 @@ public class CvsAwareIRIMapper extends AbstractScmAwareIRIMapper<CvsAwareIRIMapp
 	}
 
 	@Override
-	protected boolean isScmMapped(String url, CvsHandler cvs)
-	{
+	protected boolean isScmMapped(String url, CvsHandler cvs) {
 		return cvs.mappedCVSFiles.containsKey(url);
 	}
-
 
 }
