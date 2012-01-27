839595be447304e1bc855d5cabeb8cfd267ca65f
Heiko Dietze <hdietze@lbl.gov>
2012-01-27 00:46:55 +0000

    fix javadoc
    Former-commit-id: 52c140af4581161836d493546480b0c868b6303b

--- TermGenie/WebApplications/TermGenieJQuery/src/main/java/org/bbop/termgenie/services/visualization/TermHierarchyRenderer.java
+++ TermGenie/WebApplications/TermGenieJQuery/src/main/java/org/bbop/termgenie/services/visualization/TermHierarchyRenderer.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletContext;
 import org.bbop.termgenie.data.JsonResult;
 import org.json.rpc.server.ServletContextAware;
 
-
 public interface TermHierarchyRenderer {
 
 	/**
@@ -16,8 +15,11 @@ public interface TermHierarchyRenderer {
 	 * @param ids
 	 * @param ontology ontology name
 	 * @param servletContext (do not send in request, will be added by server)
-	 * @return location (file or url) to the image file.
+	 * @return {@link JsonResult} if successful the message contains the URL to
+	 *         the image file, otherwise it contains an error message
 	 */
 	@ServletContextAware
-	public JsonResult renderHierarchy(List<String> ids, String ontology, ServletContext servletContext);
+	public JsonResult renderHierarchy(List<String> ids,
+			String ontology,
+			ServletContext servletContext);
 }
