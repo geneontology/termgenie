0fb2a8edaf5919e561bc7d1be5b685e031366268
Heiko Dietze <hdietze@lbl.gov>
2012-03-06 21:37:20 +0000

    implement session keep alive calls to the server
    Former-commit-id: 75402eda13acbcf538e5128db8669d4c786f9ef7

--- TermGenie/WebApplications/TermGenieJQuery/src/main/webapp/js/termgenie-session.js
+++ TermGenie/WebApplications/TermGenieJQuery/src/main/webapp/js/termgenie-session.js
@@ -3,6 +3,11 @@
 	var waitForInit = [];
 	var sessionId = null;
 	
+	// Do NOT set lower than 1 minute
+	var keepAliveIntervall =  1000 * 60 * 10; // 10 minutes in milliseconds
+	
+	var lastActive = jQuery.now();
+	
 	$.extend({"TermGenieSessionManager" : function(jsonService) {
 		
 		// default create a new session or re-use existing one
@@ -14,10 +19,8 @@
 					// use this manadory call for the session id 
 					// to keep track of the last time the 
 					// session was updated.
-					// TODO Use this to reset counters for automatic keep alive pings to the server
-					// updatelastcall
+					updateLastActive();
 					callback(sessionId);
-					
 				}
 				else {
 					waitForInit.push(callback);
@@ -29,6 +32,7 @@
 			sessionId = result;
 			hasSession = true;
 			jQuery.each(waitForInit, function(index, callback){
+				updateLastActive();
 				callback(sessionId);
 			});
 			waitForInit = [];
@@ -39,11 +43,42 @@
 			jsonService.user.createSession({
 				onSuccess: function(result) {
 					setSession(result);
+					startKeepAlive();
 				},
 				onException: function(e) {
 					jQuery.logSystemError('Could not create a session. Please try reloading the page.',e);
 				}
 			});
+		}
+		
+		function updateLastActive() {
+			lastActive = jQuery.now();
+		}
+		
+		function keepAlive() {
+			if(jQuery.now() - lastActive > keepAliveIntervall){
+				if (hasSession) {
+					jsonService.user.keepSessionAlive({
+						params: [sessionId],
+						onSuccess: function(result) {
+							if (result !== true) {
+								jQuery.logUserMessage('Could not verify your session on the server. Please reload the webpage.');
+							}
+							else {
+								updateLastActive();
+							}
+						},
+						onException: function(e) {
+							jQuery.logSystemError('An error occured while trying to keep the current session alive. Please try reloading the page.',e);
+						}
+					});
+				}
+			}
+		}
+		
+		function startKeepAlive() {
+			// intervalID hold the reference for removal with clearInterval
+			var intervalID = setInterval(keepAlive, keepAliveIntervall); 
 		}
 	}});
 })(jQuery);
