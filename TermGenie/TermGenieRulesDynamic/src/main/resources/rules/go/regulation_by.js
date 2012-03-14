01ebe30c712a363ac4d0f443864e6ebb42beb5c1
Heiko Dietze <hdietze@lbl.gov>
2012-03-14 17:50:33 +0000

    update pattern for regulation_by template
    Former-commit-id: 8b92ddea8bd0b4ff38356bd66dfe1c2b9e7e9473

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/regulation_by.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/regulation_by.js
@@ -4,39 +4,37 @@ function regulation_by() {
   var go = GeneOntology;
   var p = getSingleTerm("process", go);
   var r = getSingleTerm("regulator", go);
-  var check = checkGenus(p, "GO:0008150", go);
+  var check = checkGenus(p, "GO:0008150", go); // check p is_a 'biological_process'
   if (check.isGenus() !== true) {
     error(check.error());
     return;
   }
-  check = checkGenus(r, "GO:0050789", go); // regulation of biological process
+  check = checkGenus(r, "GO:0050789", go); // check r is_a 'regulation of biological process'
   if (check.isGenus() !== true) {
     error(check.error());
     return;
   }
   subrun(r);
   
-  var negative_r = go.getOWLObjectByLabel("negative "+termname(r, go));
-  if (negative_r !== null) {
-    subrun(negative_r);
-  }
-  
-  var positive_r = go.getOWLObjectByLabel("positive "+termname(r, go));
-  if (positive_r !== null) {
-    subrun(positive_r);
-  }
+//  var negative_r = go.getOWLObjectByLabel("negative "+termname(r, go));
+//  if (negative_r !== null) {
+//    subrun(negative_r);
+//  }
+//  
+//  var positive_r = go.getOWLObjectByLabel("positive "+termname(r, go));
+//  if (positive_r !== null) {
+//    subrun(positive_r);
+//  }
   
   function subrun(regulator) {
     var label = "regulation of "+termname(p, go) + " by " + termname(regulator, go);
     var definition = "Any process that modulates the frequency, rate or extent of "+termname(p, go)+", by "+termname(regulator, go)+".";
     var synonyms = termgenie.synonyms("regulation of ", p, go, " by ", regulator, go, null, null, label);
     
-    // intentionally null, as the intended relations do not fit the pattern
-    // but the synonyms, might be of some use
     var mdef = [];
-    // var mdef = createMDef("GO_0065007 and 'is_a' some ?P and 'is_a' some ?R");
-    // mdef.addParameter('P', p, go);
-    // mdef.addParameter('P', regulator, go);
+//    var mdef = createMDef("?R and 'results_in' some ?P");
+//    mdef.addParameter('P', p, go);
+//    mdef.addParameter('P', regulator, go);
     termgenie.createTerm(label, definition, synonyms, mdef);
   };
 }
