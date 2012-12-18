bd6da33b6e8d4d36e98184397430d3b009f5edda
Heiko Dietze <hdietze@lbl.gov>
2012-12-18 22:56:58 +0000

    re-use existing code for name provider
    Former-commit-id: b69051ed90e683b8800eb40c6c1e7646ca3cb4eb

--- TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/ontology/obo/OwlGraphWrapperNameProvider.java
+++ TermGenie/TermGenieCore/src/main/java/org/bbop/termgenie/ontology/obo/OwlGraphWrapperNameProvider.java
@@ -3,30 +3,27 @@ package org.bbop.termgenie.ontology.obo;
 import java.util.Set;
 
 import org.obolibrary.oboformat.parser.OBOFormatConstants.OboFormatTag;
-import org.obolibrary.oboformat.writer.OBOFormatWriter.NameProvider;
 import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLAnnotationValue;
 import org.semanticweb.owlapi.model.OWLLiteral;
-import org.semanticweb.owlapi.model.OWLObject;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 import owltools.graph.OWLGraphWrapper;
 
 
-public class OwlGraphWrapperNameProvider implements NameProvider {
+public class OwlGraphWrapperNameProvider extends owltools.io.ParserWrapper.OWLGraphWrapperNameProvider {
 
-	private final OWLGraphWrapper wrapper;
-	private final String defaultOboNamespace;
-	
 	/**
 	 * @param wrapper
 	 */
 	public OwlGraphWrapperNameProvider(OWLGraphWrapper wrapper) {
-		super();
-		this.wrapper = wrapper;
-		final OWLAnnotationProperty defaultNamespaceProperty = wrapper.getAnnotationProperty(OboFormatTag.TAG_DEFAULT_NAMESPACE.getTag());
-		OWLOntology ontology = wrapper.getSourceOntology();
+		super(wrapper, extractDefaultNamespace(wrapper));
+	}
+
+	static String extractDefaultNamespace(OWLGraphWrapper graph) {
+		final OWLAnnotationProperty defaultNamespaceProperty = graph.getAnnotationProperty(OboFormatTag.TAG_DEFAULT_NAMESPACE.getTag());
+		OWLOntology ontology = graph.getSourceOntology();
 		String namespace = null;
 		Set<OWLAnnotation> annotations = ontology.getAnnotations();
 		for (OWLAnnotation annotation : annotations) {
@@ -38,22 +35,7 @@ public class OwlGraphWrapperNameProvider implements NameProvider {
 				}
 			}
 		}
-		defaultOboNamespace = namespace;
+		return namespace;
 	}
-
-	@Override
-	public String getName(String id) {
-		String name = null;
-		OWLObject owlObject = wrapper.getOWLObjectByIdentifier(id);
-		if (owlObject != null) {
-			name = wrapper.getLabel(owlObject);
-		}
-		return name;
-	}
-
-	@Override
-	public String getDefaultOboNamespace() {
-		return defaultOboNamespace;
-	}
-
+	
 }
