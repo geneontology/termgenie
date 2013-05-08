98136488cd79ef9178aebe43e90e791fc5eac31c
Heiko Dietze <hdietze@lbl.gov>
2013-05-08 23:58:11 +0000

    * add test case for first batto rule * fix typo in filename
    Former-commit-id: d8c4e8c56a544ad7772049839101a6a83ac7920f

--- TermGenie/WebApplications/TermGenieWebAppBatto/src/main/resources/rules/batto/eq_simple.js
+++ TermGenie/WebApplications/TermGenieWebAppBatto/src/main/resources/rules/batto/eq_simple.js
@@ -1,18 +1,18 @@
 // @requires rules/common.js
 
-function occurs_in() {
-	var e = getSingleTerm("entity", batto);
-	var q = getSingleTerm("quality", batto);
+function eq_simple() {
+	var e = getSingleTerm("entity", Batto);
+	var q = getSingleTerm("quality", Batto);
 
-	var label = termname(e, batto) + ' ' + termname(q, batto);
+	var label = termname(e, Batto) + ' ' + termname(q, Batto);
 	var definition = "Any measurable or observable characteristic related to the "
-			+ termname(q, batto) + " of " + refname(e, batto) + ".";
+			+ termname(q, Batto) + " of " + refname(e, Batto) + ".";
 
 	// TODO synonyms: E Q 'trait'
 	var synonyms = null;
 	
 	var mdef = createMDef("'biological attribute' and affects_quality some ?Q and attribute_of some ?E");
-	mdef.addParameter('E', e, batto);
-	mdef.addParameter('Q', q, batto);
+	mdef.addParameter('E', e, Batto);
+	mdef.addParameter('Q', q, Batto);
 	createTerm(label, definition, synonyms, mdef);
 }
