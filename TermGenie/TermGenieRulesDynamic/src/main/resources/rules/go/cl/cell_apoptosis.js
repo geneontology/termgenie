5980b8867d905eff09a1e5015d19bc7c376835c4
Heiko Dietze <hdietze@lbl.gov>
2013-05-30 22:50:57 +0000

    def change
    Former-commit-id: e86ae234d0e33ecb9d4dfa2cbb9f8fed67e7565c

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cl/cell_apoptosis.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cl/cell_apoptosis.js
@@ -5,7 +5,7 @@ function cell_apoptosis() {
   var c = getSingleTerm("celltype", go);
   
   var label = termname(c, go) + " apoptotic process";
-  var definition = "Any apoptotic process that takes place in "+termname(c, go)+".";
+  var definition = "Any apoptotic process in "+refname(c, go)+".";
   var synonyms = termgenie.synonyms(null, c, go, " apoptosis", null, label);
   var mdef = createMDef("'apoptotic process' and 'occurs in' some ?C");
   mdef.addParameter('C', c, go);
