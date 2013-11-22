398dfe9a22fb56eead349d73993c7428e3449ee9
Heiko Dietze <hdietze@lbl.gov>
2013-11-22 00:10:39 +0000

    new template: single-multi-organism process
    Former-commit-id: 7709fb8c540c942ad70834bb48b140ad70d3b500

---
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/single_multi_organism_process.js
@@ -0,0 +1,64 @@
+// @requires rules/common.js
+
+function single_multi_organism_process() {
+	var go = GeneOntology;
+	var p = getSingleTerm("process", go);
+	
+	var prefixes = getInputs("process");
+	if (!prefixes || prefixes === null || prefixes.length === 0) {
+		error("Could not create a term for X, as neither single-organism nor multi-organism was selected");
+		return;
+	}
+	
+	var check = checkGenus(p, "GO:0008150", go);
+	if (check.isGenus() !== true) {
+		error(check.error());
+		return;
+	}
+  
+	var count = 0;
+	var name = termname(p, go);
+	var cRefName = capitalizeFirstLetter(termgenie.refname(p, go));
+	// single-organism process
+	if (termgenie.contains(prefixes, "single-organism")) {
+		var label = "single-organism " + name;
+		var definition = cRefName+" which involves only one organism.";
+		var synonyms = termgenie.addSynonym(label, null, "single-organism ", name, "", "EXACT");
+		var mdef = createMDef("?P and 'bearer_of' some PATO_0002487");
+		mdef.addParameter('P', p, go);
+		var success = createTerm(label, definition, synonyms, mdef);
+		if (success === true) {
+			count += 1;
+		}
+		else {
+			return;
+		}
+	}
+	
+	// multi-organism process
+	if (termgenie.contains(prefixes, "multi-organism")) {
+		var label = "multi-organism " + name;
+		var definition = cRefName + " which involves another organism of the same or different species.";
+		var synonyms = termgenie.addSynonym(label, null, "multi-organism ", name, "", "EXACT");
+		var mdef = createMDef("?P and 'bearer_of' some PATO_0002486");
+		mdef.addParameter('P', p, go);
+		var success = createTerm(label, definition, synonyms, mdef);
+		if (success === true) {
+			count += 1;
+		}
+		else {
+			return;
+		}
+	}
+  
+	if (count === 0) {
+		error("Could not create a term for X, as neither single-organism nor multi-organism was selected");
+	}
+	
+	
+	
+	function capitalizeFirstLetter(string)
+	{
+	    return string.substring(0,1).toUpperCase() + string.substring(1);
+	}
+}
--- TermGenie/WebApplications/TermGenieWebAppGO/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppGOContextListener.java
+++ TermGenie/WebApplications/TermGenieWebAppGO/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppGOContextListener.java
@@ -124,6 +124,7 @@ public class TermGenieWebAppGOContextListener extends AbstractTermGenieContextLi
         specialPatterns.put("chemical_import_into", 1); // relations and chebi
         specialPatterns.put("cc_assembly_disassembly", 1); // relations
         specialPatterns.put("protein_complex_by_activity", 1); // relations
+        specialPatterns.put("single_multi_organism_process", 1); // relation and PATO term as differentia
         
 		String repositoryURL = "svn+ssh://ext.geneontology.org/share/go/svn/trunk/ontology";
 		String remoteTargetFile = "editors/gene_ontology_write.obo";
