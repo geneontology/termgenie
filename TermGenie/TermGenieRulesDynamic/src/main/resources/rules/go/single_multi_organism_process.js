6da31e2a330c126ca0d8bea377294766cd78554f
Heiko Dietze <hdietze@lbl.gov>
2013-12-02 18:23:28 +0000

    update def for multi-organism process; add synonyms without a dash
    Former-commit-id: 4d8330b7ddb9c42750db2e9b83a1457dfb25933b

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/single_multi_organism_process.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/single_multi_organism_process.js
@@ -23,7 +23,7 @@ function single_multi_organism_process() {
 	if (termgenie.contains(prefixes, "single-organism")) {
 		var label = "single-organism " + name;
 		var definition = cRefName+" which involves only one organism.";
-		var synonyms = termgenie.addSynonym(label, null, "single-organism ", name, "", "EXACT");
+		var synonyms = termgenie.addSynonym(label, null, "single organism ", name, "", "EXACT");
 		var mdef = createMDef("?P and 'bearer_of' some PATO_0002487");
 		mdef.addParameter('P', p, go);
 		var success = createTerm(label, definition, synonyms, mdef);
@@ -38,8 +38,8 @@ function single_multi_organism_process() {
 	// multi-organism process
 	if (termgenie.contains(prefixes, "multi-organism")) {
 		var label = "multi-organism " + name;
-		var definition = cRefName + " which involves another organism of the same or different species.";
-		var synonyms = termgenie.addSynonym(label, null, "multi-organism ", name, "", "EXACT");
+		var definition = cRefName + " which involves another organism.";
+		var synonyms = termgenie.addSynonym(label, null, "multi organism ", name, "", "EXACT");
 		var mdef = createMDef("?P and 'bearer_of' some PATO_0002486");
 		mdef.addParameter('P', p, go);
 		var success = createTerm(label, definition, synonyms, mdef);
