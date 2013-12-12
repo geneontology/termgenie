f543d0e289aa95f0cfec4e740ab873fa6c4eacf8
Heiko Dietze <hdietze@lbl.gov>
2013-12-12 20:47:24 +0000

    fix ontology name
    Former-commit-id: 389863de144cdaa066c90ef21a42b8daf81610c2

--- TermGenie/WebApplications/TermGenieWebAppBatto/src/main/resources/rules/batto/eq_simple.js
+++ TermGenie/WebApplications/TermGenieWebAppBatto/src/main/resources/rules/batto/eq_simple.js
@@ -1,19 +1,19 @@
 // @requires rules/common.js
 
 function eq_simple() {
-	var e = getSingleTerm("entity", Batto);
-	var q = getSingleTerm("quality", Batto);
+	var e = getSingleTerm("entity", OBA);
+	var q = getSingleTerm("quality", OBA);
 
-	var label = termname(e, Batto) + ' ' + termname(q, Batto);
+	var label = termname(e, OBA) + ' ' + termname(q, OBA);
 	var definition = "Any measurable or observable characteristic related to the "
-			+ termname(q, Batto) + " of " + refname(e, Batto) + ".";
+			+ termname(q, OBA) + " of " + refname(e, OBA) + ".";
 
 	var synonyms = null;
 	// String label, List<ISynonym> results, String prefix, String infix, String suffix, String scope
 	synonyms = termgenie.addSynonym(label, synonyms, null, label, ' trait', 'EXACT');
 	
 	var mdef = createMDef("'biological attribute' and affects_quality some ?Q and attribute_of some ?E");
-	mdef.addParameter('E', e, Batto);
-	mdef.addParameter('Q', q, Batto);
+	mdef.addParameter('E', e, OBA);
+	mdef.addParameter('Q', q, OBA);
 	createTerm(label, definition, synonyms, mdef);
 }
