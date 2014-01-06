7a734d66d74e8bb3863611db22b72fdf6e62eefb
Heiko Dietze <hdietze@lbl.gov>
2014-01-06 21:35:14 +0000

    more cl configuration stuff
    Former-commit-id: 38e96a9015186f1cf33ee62e5e6e04233eef6383

--- TermGenie/WebApplications/TermGenieWebAppCL/src/main/resources/rules/cl/metazoan_location_specific_cell.js
+++ TermGenie/WebApplications/TermGenieWebAppCL/src/main/resources/rules/cl/metazoan_location_specific_cell.js
@@ -2,17 +2,17 @@
 
 function metazoan_location_specific_cell() {
   var c = getSingleTerm("cell", CL);
-  var a = getSingleTerm("location", Uberon);
+  var a = getSingleTerm("location", CL);
   if (c == null || a == null) {
     // check branch
     error("The specified terms do not correspond to the pattern");
     return;
   }
-  var label = termname(a, Uberon) + " " + termname(c, CL);
-  var definition = "Any "+termname(c, CL)+" that is part of a "+termname(a, Uberon)+".";
+  var label = termname(a, CL) + " " + termname(c, CL);
+  var definition = "Any "+termname(c, CL)+" that is part of a "+termname(a, CL)+".";
   var synonyms = null; // TODO
   var mdef = createMDef("?C and 'part_of' some ?A");
   mdef.addParameter('C', c, CL);
-  mdef.addParameter('A', a, Uberon);
+  mdef.addParameter('A', a, CL);
   createTerm(label, definition, synonyms, mdef);
 }
