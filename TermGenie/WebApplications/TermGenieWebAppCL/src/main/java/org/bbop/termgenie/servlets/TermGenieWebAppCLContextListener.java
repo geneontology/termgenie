6e1eed87acad71733e2f7b7060b038022191c76d
Heiko Dietze <hdietze@lbl.gov>
2014-01-13 23:53:18 +0000

    update config for CL, including * svn commit * ids range settings
    Former-commit-id: fb52749a597d1a5d66219c89d6468e12ed7d4a97

--- TermGenie/WebApplications/TermGenieWebAppCL/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppCLContextListener.java
+++ TermGenie/WebApplications/TermGenieWebAppCL/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppCLContextListener.java
@@ -1,11 +1,35 @@
 package org.bbop.termgenie.servlets;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.bbop.termgenie.core.ioc.IOCModule;
-import org.bbop.termgenie.ontology.impl.XMLReloadingOntologyModule;
+import org.bbop.termgenie.mail.MailHandler;
+import org.bbop.termgenie.mail.SimpleMailHandler;
+import org.bbop.termgenie.mail.review.DefaultReviewMailHandlerModule;
+import org.bbop.termgenie.ontology.AdvancedPersistenceModule;
+import org.bbop.termgenie.ontology.impl.SvnAwareXMLReloadingOntologyModule;
+import org.bbop.termgenie.ontology.svn.CommitSvnUserPasswdModule;
+import org.bbop.termgenie.presistence.PersistenceBasicModule;
 import org.bbop.termgenie.rules.XMLDynamicRulesModule;
+import org.bbop.termgenie.services.DefaultTermCommitServiceImpl;
+import org.bbop.termgenie.services.TermCommitService;
+import org.bbop.termgenie.services.TermGenieServiceModule;
 import org.bbop.termgenie.services.permissions.UserPermissionsModule;
+import org.bbop.termgenie.services.review.TermCommitReviewServiceModule;
+import org.semanticweb.owlapi.model.IRI;
 
 public class TermGenieWebAppCLContextListener extends AbstractTermGenieContextListener {
+	
+	private final static Logger logger = Logger.getLogger(TermGenieWebAppCLContextListener.class);
 
 	public TermGenieWebAppCLContextListener() {
 		super("TermGenieWebAppCLConfigFile");
@@ -16,8 +40,35 @@ public class TermGenieWebAppCLContextListener extends AbstractTermGenieContextLi
 	}
 	
 	@Override
+	protected TermGenieServiceModule getServiceModule() {
+		return new TermGenieServiceModule(applicationProperties) {
+
+			@Override
+			protected void bindTermCommitService() {
+				bind(TermCommitService.class, DefaultTermCommitServiceImpl.class);
+			}
+			
+			@Override
+			public String getModuleName() {
+				return "TermGenieCL-TermGenieServiceModule";
+			}
+		};
+	}
+	
+	@Override
 	protected IOCModule getOntologyModule() {
-		return new XMLReloadingOntologyModule("ontology-configuration_cl.xml", null, applicationProperties);
+		String configFile = "ontology-configuration_cl.xml";
+		String repositoryURL = "https://cell-ontology.googlecode.com/svn/trunk/src/ontology";
+		String workFolder = null; // no default value
+		String svnUserName = null; // no default value
+		boolean loadExternal = false;
+		String catalogXML = "catalog-v001.xml";
+		
+		// no special handling of certain IRIs
+		Map<IRI, String> mappedIRIs = Collections.emptyMap(); 
+		List<String> ignoreIRIs = Arrays.asList();
+		
+		return SvnAwareXMLReloadingOntologyModule.createUsernamePasswordSvnModule(configFile, applicationProperties, repositoryURL, mappedIRIs, catalogXML, workFolder, svnUserName, loadExternal, ignoreIRIs);
 	}
 
 	@Override
@@ -27,4 +78,95 @@ public class TermGenieWebAppCLContextListener extends AbstractTermGenieContextLi
 		return new XMLDynamicRulesModule("termgenie_rules_cl.xml", useIsInferred, assertInferences, applicationProperties);
 	}
 
+	@Override
+	protected IOCModule getCommitModule() {
+        
+		String repositoryURL = "https://cell-ontology.googlecode.com/svn/trunk/src/ontology";
+		String remoteTargetFile = "cl-edit.owl";
+		String svnUserName = null; // no default value
+		boolean loadExternal = false;
+		
+		return CommitSvnUserPasswdModule.createOwlModule(repositoryURL, remoteTargetFile, svnUserName, applicationProperties, loadExternal);
+	}
+	
+	@Override
+	protected TermCommitReviewServiceModule getCommitReviewWebModule() {
+		return new TermCommitReviewServiceModule(true, applicationProperties) {
+
+			@Override
+			public String getModuleName() {
+				return "TermGenieCL-TermCommitReviewServiceModule";
+			}
+			
+			@Override
+			protected void bindEnabled() {
+				// TODO bind and OWL specific review service!!
+//				bind(TermCommitReviewService.class, OboTermCommitReviewServiceImpl.class);
+			}
+		};
+	}
+	
+	@Override
+	protected Collection<IOCModule> getAdditionalModules() {
+		List<IOCModule> modules = new ArrayList<IOCModule>();
+		PersistenceBasicModule basicPersistenceModule = getBasicPersistenceModule();
+		if (basicPersistenceModule != null) {
+			modules.add(basicPersistenceModule);
+		}
+		// commit history and ontology id store
+		AdvancedPersistenceModule advancedPersistenceModule = getAdvancedPersistenceModule();
+		if (advancedPersistenceModule != null) {
+			modules.add(advancedPersistenceModule);
+		}
+		return modules;
+	}
+
+	protected AdvancedPersistenceModule getAdvancedPersistenceModule() {
+		return new AdvancedPersistenceModule("CL-ID-Manager-Primary", 
+				"ids/cl-id-manager-primary.conf",
+				"CL-ID-Manager-Secondary", 
+				"ids/cl-id-manager-secondary.conf", 
+				applicationProperties);
+	}
+
+	protected PersistenceBasicModule getBasicPersistenceModule() {
+		try {
+			// basic persistence
+			String dbFolderString = IOCModule.getProperty("TermGenieWebappCLDatabaseFolder", applicationProperties);
+			File dbFolder;
+			if (dbFolderString != null && !dbFolderString.isEmpty()) {
+				dbFolder = new File(dbFolderString);
+			}
+			else {
+				dbFolder = new File(FileUtils.getUserDirectory(), "termgenie-cl-db");
+			}
+			dbFolder = dbFolder.getCanonicalFile();
+			logger.info("Using db folder: "+dbFolder);
+			FileUtils.forceMkdir(dbFolder);
+			return new PersistenceBasicModule(dbFolder, applicationProperties);
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+	
+	@Override
+	protected IOCModule getReviewMailHandlerModule() {
+		
+		return new DefaultReviewMailHandlerModule(applicationProperties, "help@go.termgenie.org", "CL TermGenie") {
+			
+			@Override
+			protected MailHandler provideMailHandler() {
+				return new SimpleMailHandler("smtp.lbl.gov");
+			}
+		};
+	}
+	
+//	@Override
+//	protected IOCModule getFreeFormTermModule() {
+//		List<String> oboNamespaces = null;
+//		String defaultOntology = "default_cl";
+//		boolean addSubsetTag = false;
+//		String subset = null;
+//		return new FreeFormTermServiceModule(applicationProperties, addSubsetTag , defaultOntology, oboNamespaces, subset);
+//	}
 }
--- TermGenie/WebApplications/TermGenieWebAppCLTest/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppCLTestContextListener.java
+++ TermGenie/WebApplications/TermGenieWebAppCLTest/src/main/java/org/bbop/termgenie/servlets/TermGenieWebAppCLTestContextListener.java
@@ -1,6 +1,82 @@
 package org.bbop.termgenie.servlets;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.bbop.termgenie.core.ioc.IOCModule;
+import org.bbop.termgenie.mail.review.NoopReviewMailHandler;
+import org.bbop.termgenie.ontology.impl.SvnAwareXMLReloadingOntologyModule;
+import org.bbop.termgenie.ontology.svn.CommitSvnAnonymousModule;
+import org.bbop.termgenie.presistence.PersistenceBasicModule;
+import org.semanticweb.owlapi.model.IRI;
+
 
 public class TermGenieWebAppCLTestContextListener extends TermGenieWebAppCLContextListener {
 
+	private final String localSVNFolder;
+	private final String catalogXML = "catalog-v001.xml";
+	private final boolean loadExternal = false;
+	
+	private final boolean cleanDownloadCache = false;
+	
+	
+	public TermGenieWebAppCLTestContextListener() {
+		try {
+			localSVNFolder = "file://"+new File("./work/svn").getCanonicalPath();
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+	
+	@Override
+	protected IOCModule getOntologyModule() {
+		try {
+			String configFile = "ontology-configuration_cl.xml";
+			File localSVNCache = new File("./work/read-only-svn-checkout").getCanonicalFile();
+			localSVNCache.mkdirs();
+			if (cleanDownloadCache) {
+				FileUtils.cleanDirectory(localSVNCache);
+			}
+			
+			String fileCache = new File("./work/termgenie-download-cache").getCanonicalPath();
+
+			// no special handling
+			List<String> ignoreIRIs = Collections.emptyList();
+			Map<IRI, String> mappedIRIs = Collections.emptyMap();
+			
+			return SvnAwareXMLReloadingOntologyModule.createAnonymousSvnModule(configFile , applicationProperties, localSVNFolder, mappedIRIs, catalogXML, localSVNCache.getAbsolutePath(), fileCache, loadExternal, ignoreIRIs);
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	
+	@Override
+	protected IOCModule getCommitModule() {
+		return CommitSvnAnonymousModule.createOboModule(localSVNFolder, "extensions/bio-attributes.obo", applicationProperties, Collections.<String>emptyList(), loadExternal);
+	}
+	
+	@Override
+	protected PersistenceBasicModule getBasicPersistenceModule() {
+		try {
+			// basic persistence
+			File dbFolder = new File("./work/termgenie-cl-db").getCanonicalFile();
+			Logger.getLogger(getClass()).info("Setting db folder to: "+dbFolder.getAbsolutePath());
+			FileUtils.forceMkdir(dbFolder);
+			return new PersistenceBasicModule(dbFolder, applicationProperties);
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	@Override
+	protected IOCModule getReviewMailHandlerModule() {
+		// deactivate e-mail
+		return new NoopReviewMailHandler.NoopReviewMailHandlerModule(applicationProperties);
+	}
 }
