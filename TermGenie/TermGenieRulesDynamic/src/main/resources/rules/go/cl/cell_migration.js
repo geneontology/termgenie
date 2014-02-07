c9ccaf9f83ae4fb7cd62323d331d4c2883c990e7
Heiko Dietze <hdietze@lbl.gov>
2014-02-07 20:45:42 +0000

    minor edits for cell migration template
    Former-commit-id: 96e9af3757b22eb6d500204b33db8984a3d5a8bd

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cl/cell_migration.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cl/cell_migration.js
@@ -5,7 +5,7 @@ function cell_migration() {
   var c = getSingleTerm("celltype", go);
   
   var label = termname(c, go) + " migration";
-  var definition = "The movement of "+refname(c, go)+" within or between different tissues and organs of the body.";
+  var definition = "The orderly movement of a "+refname(c, go)+" from one site to another.";
   var synonyms = null;
   var mdef = createMDef("'cell migration' and 'alters location of' some ?C");
   mdef.addParameter('C', c, go);
