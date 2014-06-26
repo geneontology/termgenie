3949fa62cf72e9f64810583d4108e65646e70ffe
Heiko Dietze <hdietze@lbl.gov>
2014-06-26 17:48:09 +0000

    create empty sub projects for HP termgenie
    Former-commit-id: 389c8a82855272066a22fbf9977fe07e798f106e

---
+++ TermGenie/WebApplications/TermGenieWebAppHPTest/prepare/create_svn.sh
@@ -0,0 +1,10 @@
+rm -rf svn
+rm -rf first_checkout
+mkdir svn
+SVN_TARGET_DIR="$PWD/svn"
+svnadmin create $SVN_TARGET_DIR
+echo $SVN_TARGET_DIR
+svn import -m "initial import" ontology "file://$SVN_TARGET_DIR"
+rm -rf ../work/svn
+cp -r svn ../work
+
