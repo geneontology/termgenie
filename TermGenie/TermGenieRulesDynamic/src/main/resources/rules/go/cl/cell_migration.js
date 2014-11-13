5aadc7de8286f437dab2e6952b6c989046abb606
Heiko Dietze <hdietze@lbl.gov>
2014-11-13 17:03:42 +0000

    remove TODO
    Former-commit-id: e490536d4a70046a052c770f2c7d891f9c85eb49

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cl/cell_migration.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cl/cell_migration.js
@@ -7,7 +7,6 @@ function cell_migration() {
   var label = termname(c, go) + " migration";
   var definition = "The orderly movement of "+refname(c, go)+" from one site to another.";
   var synonyms = null;
-  // TODO unify relation with GO, wait for appropriate relation in RO 'results in movement of'?
   var mdef = createMDef("'cell migration' and 'results in movement of' some ?C"); 
   mdef.addParameter('C', c, go);
   createTerm(label, definition, synonyms, mdef);
