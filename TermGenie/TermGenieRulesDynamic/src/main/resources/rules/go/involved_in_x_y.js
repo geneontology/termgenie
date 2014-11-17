9498303a3293912ca88c503eb2f37ae85d307b8e
Heiko Dietze <hdietze@lbl.gov>
2014-11-17 22:42:45 +0000

    replace 'part_of' with 'part of' to be inline with the ontology
    Former-commit-id: 8d61dbca93c9ce5d3444affb8c3cdb3872ef1455

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/involved_in_x_y.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/involved_in_x_y.js
@@ -17,7 +17,7 @@ function involved_in_x_y(x, y){
   var label = termname(p, go) + " involved in " + termname(w, go);
   var definition = "Any "+termname(p, go)+" that is involved in "+termname(w, go)+".";
   var synonyms = termgenie.synonyms(null, p, go, " involved in ", w, go, null, null, label);
-  var mdef = createMDef("?P and 'part_of' some ?W");
+  var mdef = createMDef("?P and 'part of' some ?W");
   mdef.addParameter('P', p, go);
   mdef.addParameter('W', w, go);
   createTerm(label, definition, synonyms, mdef);
--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/part_of_cell_component.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/part_of_cell_component.js
@@ -18,7 +18,7 @@ function part_of_cell_component() {
 	var definition = "Any " + termname(p, go) + " that is part of a "
 			+ termname(w, go) + ".";
 	var synonyms = termgenie.synonyms(null, p, go, " of ", w, go, null, null, label);
-	var mdef = createMDef("?P and 'part_of' some ?W");
+	var mdef = createMDef("?P and 'part of' some ?W");
 	mdef.addParameter('P', p, go);
 	mdef.addParameter('W', w, go);
 	createTerm(label, definition, synonyms, mdef);
