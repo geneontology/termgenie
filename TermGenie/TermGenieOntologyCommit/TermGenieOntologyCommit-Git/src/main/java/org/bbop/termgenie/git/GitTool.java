68e5b3270de99ab0261dd5b01c1ed7cea4c77eb7
hdietze <hdietze@lbl.gov>
2015-04-13 14:20:34 -0700

    minor cleanup

--- TermGenie/TermGenieOntologyCommit/TermGenieOntologyCommit-Git/src/main/java/org/bbop/termgenie/git/GitTool.java
+++ TermGenie/TermGenieOntologyCommit/TermGenieOntologyCommit-Git/src/main/java/org/bbop/termgenie/git/GitTool.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 import org.apache.log4j.Logger;
 import org.bbop.termgenie.core.process.ProcessState;
