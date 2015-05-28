57a6ab644b328b56cca2361ff834c89eb0146a36
hdietze <hdietze@lbl.gov>
2015-05-28 09:40:11 -0700

    use less specific genus, as property guarantees proper inferences

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cc_organization.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/cc_organization.js
@@ -17,7 +17,7 @@ function cc_organization() {
 	var definition = "A process that is carried out at the cellular level which results in the assembly, arrangement of constituent parts, or disassembly of " + cRefName + "."; 
 
 	var synonyms = termgenie.synonyms(null, ['EXACT'], c, go, [' organization'], label);
-	var mdef = createMDef("GO_0016043 and 'results_in_organization_of' some ?C");
+	var mdef = createMDef("GO_0009987 and 'results_in_organization_of' some ?C");
 	mdef.addParameter('C', c, go);
 	createTerm(label, definition, synonyms, mdef);
 }
