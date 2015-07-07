9a2d67aadaaab0def1ea2016b349ffbc16590f2f
Heiko Dietze <hdietze@lbl.gov>
2015-07-07 09:15:17 -0700

    cellular component binding -> macromolecular complex binding

--- TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/macromolecular_complex_binding.js
+++ TermGenie/TermGenieRulesDynamic/src/main/resources/rules/go/macromolecular_complex_binding.js
@@ -1,10 +1,10 @@
 // @requires rules/common.js
 
-function cc_binding() {
+function macromolecular_complex_binding() {
 	var go = GeneOntology;
-	var c = getSingleTerm("component", go);
+	var c = getSingleTerm("complex", go);
 	
-	var check = checkGenus(c, "GO:0005575", go); // cellular_component
+	var check = checkGenus(c, "GO:0032991", go); // macromolecular complex
 	if (check.isGenus() !== true) {
 		error(check.error());
 		return;
